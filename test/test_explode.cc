f2de66715ab92319febab7e037641500f756ee58
menonfled <elddiry@gmail.com>
2020-09-11 00:39:48 +0900

    explodeおよびimplode関数のテストケースを追加

---
+++ test/test_explode.cc
@@ -0,0 +1,28 @@
+﻿#include "menon/string.hh"
+#include <boost/core/lightweight_test.hpp>
+
+int main()
+{
+  using namespace std::literals::string_view_literals;
+  {
+    auto r = menon::explode(",", "abc,de,,fg");
+    std::vector t = { "abc"sv, "de"sv, ""sv, "fg"sv };
+    BOOST_TEST_ALL_EQ(r.cbegin(), r.cend(), t.cbegin(), t.cend());
+  }
+  {
+    auto r = menon::explode(",", "abc,de,,fg", 2);
+    std::vector t = { "abc"sv, "de,,fg"sv };
+    BOOST_TEST_ALL_EQ(r.cbegin(), r.cend(), t.cbegin(), t.cend());
+  }
+  {
+    auto r = menon::explode(",", "abc,de,,fg", -2);
+    std::vector t = { "abc"sv, "de"sv };
+    BOOST_TEST_ALL_EQ(r.cbegin(), r.cend(), t.cbegin(), t.cend());
+  }
+  {
+    auto r = menon::explode(',', "abc,de,,fg");
+    std::vector t = { "abc"sv, "de"sv, ""sv, "fg"sv };
+    BOOST_TEST_ALL_EQ(r.cbegin(), r.cend(), t.cbegin(), t.cend());
+  }
+  return boost::report_errors();
+}
---
+++ test/test_implode.cc
@@ -0,0 +1,21 @@
+﻿#include "menon/string.hh"
+#include <boost/core/lightweight_test.hpp>
+#include <vector>
+
+int main()
+{
+  using namespace std::literals::string_view_literals;
+  using namespace std::literals::string_literals;
+  {
+    auto t = { "abc"s, "def"s, "ghi"s };
+    BOOST_TEST_EQ(menon::implode(",", t), "abc,def,ghi"s);
+  }
+  {
+    BOOST_TEST_EQ(menon::implode(",", { "abc"s, "def"s, "ghi"s }), "abc,def,ghi"s);
+  }
+  {
+    std::vector t = { "abc"s, "def"s, "ghi"s };
+    BOOST_TEST_EQ(menon::implode(",", t.begin(), t.end()), "abc,def,ghi"s);
+  }
+  return boost::report_errors();
+}
