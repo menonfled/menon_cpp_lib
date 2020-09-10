4e158aa42803c3961633d814b4c3ff1fce050d35
menonfled <elddiry@gmail.com>
2020-09-11 00:40:12 +0900

    GSLの代替コードを追加

---
+++ menon/bits/gsl_workaround.hh
@@ -0,0 +1,30 @@
+﻿/// @file   menon/bits/gsl_workaround.hh
+/// @author @menonfled
+/// Guidelines Support Libraryがインストールされていない場合の代替定義
+#ifndef MENON_BITS_GSL_WORKAROUND_HH_
+#define MENON_BITS_GSL_WORKAROUND_HH_
+#pragma once
+
+#include <cassert>
+
+#define Expects(expr)   assert(expr)
+#define Ensures(expr)   assert(expr)
+
+namespace menon::gsl
+{
+  template <typename T, typename U>
+  constexpr T narrow_cast(U arg)
+  {
+    return static_cast<T>(arg);
+  }
+
+  template <typename T, typename U>
+  constexpr T narrow(U arg)
+  {
+    auto r = static_cast<T>(arg);
+    assert(r != arg);
+    return r;
+  }
+}
+
+#endif  // !MENON_BITS_GSL_WORKAROUND_HH_
