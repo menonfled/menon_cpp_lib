c020696a78cb2ab80c545c60325018d52fd3cfac
menonfled <elddiry@gmail.com>
2020-09-12 01:12:39 +0900

    explodeを改良

--- menon/bits/explode.hh
+++ menon/bits/explode.hh
@@ -7,24 +7,24 @@
 
 #include "menon/bits/sv.hh"
 #include <vector>
+#include <iterator>
 #include <climits>
 
 namespace menon
 {
   namespace detail
   {
-    template <typename Char, typename Traits>
-    auto explode_helper(std::basic_string_view<Char, Traits> delimiter, std::basic_string_view<Char, Traits> str, int limit)
-      -> std::vector<std::basic_string_view<Char, Traits>>
+    template <typename Char, typename Traits, typename Target>
+    auto explode_helper(std::basic_string_view<Char, Traits> delimiter, std::basic_string_view<Char, Traits> str, Target& r, int limit)
     {
       using sv_type = std::basic_string_view<Char, Traits>;
       using size_type = typename sv_type::size_type;
+      using value_type = typename Target::value_type;
       size_type pos = 0;
       size_type pos2 = 0;
       size_type npos = sv_type::npos;
       size_type delimiter_size = delimiter.size();
 
-      std::vector<sv_type> r;
       if (limit == 0)
         limit = 1;
 
@@ -34,22 +34,22 @@ namespace menon
         while (pos2 != npos)
         {
           pos2 = str.find(delimiter, pos);
-          auto element = str.substr(pos, pos2 - pos);
-          r.push_back(element);
+          r.push_back(value_type(str.substr(pos, pos2 - pos)));
           pos = pos2 + delimiter_size;
 
           if (limit > 0 && ++n >= limit)
           {
-            r.push_back(str.substr(pos));
+            r.push_back(value_type(str.substr(pos)));
             break;
           }
         }
         if (limit < 0)
         {
-          r.erase(r.begin() + r.size() + limit, r.end());
+          auto it = r.begin();
+          std::advance(it, r.size() + limit);
+          r.erase(it, r.end());
         }
       }
-      return r;
     }
   }
 
@@ -60,14 +60,39 @@ namespace menon
   /// @return     分割した文字列をstring_viewのvectorとして返す。
   /// この関数はPHPのexplodeからの借用である。
   /// 
-  /// limitが正の場合、結果には最大limitの要素が含まれ、その最後の要素にはstrの残りの部分が含まれる。
-  /// limitが負の場合、最後の-limit個を除くすべての要素が返される。
-  /// limitが0の場合、1を指定したものとして扱われる。
+  /// - limitが正の場合、結果には最大limitの要素が含まれ、その最後の要素にはstrの残りの部分が含まれる。
+  /// - limitが負の場合、最後の-limit個を除くすべての要素が返される。
+  /// - limitが0の場合、1を指定したものとして扱われる。
   template <typename T, typename U>
   auto explode(T delimiter, U str, int limit = INT_MAX)
   {
     using ::menon::sv;
-    return detail::explode_helper(sv(delimiter), sv(str), limit);
+    auto s = sv(str);
+    std::vector<decltype(s)> r;
+    detail::explode_helper(sv(delimiter), s, r, limit);
+    return r;
+  }
+
+  /// 文字列を文字（列）により分割する
+  /// @param[in]  delimiter 区切り文字（列）
+  /// @param[in]  str       分割対象の文字列
+  /// @param[in]  r         分割結果の格納先
+  /// @param[in]  limit     結果の最大要素
+  /// @return     rを返す。
+  /// 
+  /// - limitが正の場合、結果には最大limitの要素が含まれ、その最後の要素にはstrの残りの部分が含まれる。
+  /// - limitが負の場合、最後の-limit個を除くすべての要素が返される。
+  /// - limitが0の場合、1を指定したものとして扱われる。
+  ///
+  /// 分割結果の格納先をrで指定することができる。
+  /// これにより、string_viewのvectorだけでなく、stringのvectorやlistなどを用いることもできる。
+  template <typename T, typename U, typename Target>
+  auto explode(T delimiter, U str, Target& r, int limit = INT_MAX)
+    -> Target&
+  {
+    using ::menon::sv;
+    detail::explode_helper(sv(delimiter), sv(str), r, limit);
+    return r;
   }
 }
 
--- test/test_explode.cc
+++ test/test_explode.cc
@@ -1,5 +1,8 @@
 ﻿#include "menon/string.hh"
 #include <boost/core/lightweight_test.hpp>
+#include <vector>
+#include <deque>
+#include <list>
 
 int main()
 {
@@ -23,6 +26,35 @@ int main()
     auto r = menon::explode(',', "abc,de,,fg");
     std::vector t = { "abc"sv, "de"sv, ""sv, "fg"sv };
     BOOST_TEST_ALL_EQ(r.cbegin(), r.cend(), t.cbegin(), t.cend());
+  }
+
+  {
+    std::vector<std::string> r;
+    auto& r2 = menon::explode(",", "abc,de,,fg", r);
+    std::vector t = { "abc"sv, "de"sv, ""sv, "fg"sv };
+    BOOST_TEST_ALL_EQ(r.cbegin(), r.cend(), t.cbegin(), t.cend());
+    BOOST_TEST_EQ(&r, &r2);
+  }
+  {
+    std::deque<std::string> r;
+    auto& r2 = menon::explode(",", "abc,de,,fg", r, 2);
+    std::vector t = { "abc"sv, "de,,fg"sv };
+    BOOST_TEST_ALL_EQ(r.cbegin(), r.cend(), t.cbegin(), t.cend());
+    BOOST_TEST_EQ(&r, &r2);
+  }
+  {
+    std::list<std::string> r;
+    auto& r2 = menon::explode(",", "abc,de,,fg", r, -2);
+    std::vector t = { "abc"sv, "de"sv };
+    BOOST_TEST_ALL_EQ(r.cbegin(), r.cend(), t.cbegin(), t.cend());
+    BOOST_TEST_EQ(&r, &r2);
+  }
+  {
+    std::vector<std::string_view> r;
+    auto& r2 = menon::explode(',', "abc,de,,fg", r);
+    std::vector t = { "abc"sv, "de"sv, ""sv, "fg"sv };
+    BOOST_TEST_ALL_EQ(r.cbegin(), r.cend(), t.cbegin(), t.cend());
+    BOOST_TEST_EQ(&r, &r2);
   }
   return boost::report_errors();
 }
