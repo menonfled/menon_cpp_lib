fb5a43f018a1bb4bdaec4cdf50a127a83b8620f7
menonfled <elddiry@gmail.com>
2020-09-14 00:02:34 +0900

    algorithm.hhを追加

---
+++ menon/algorithm.hh
@@ -0,0 +1,9 @@
+/// @file   menon/algorithm.hh
+/// @author @menonfled
+/// アルゴリズム関数の宣言・定義
+#ifndef MENON_ALGORITHM_HH_
+#define MENON_ALGORITHM_HH_
+#pragma once
+
+
+#endif  // !MENON_ALGORITHM_HH_
