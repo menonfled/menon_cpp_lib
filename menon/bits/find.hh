a4236833b6c5481c4685c9ae9b758fe05164db08
menonfled <elddiry@gmail.com>
2020-09-14 00:38:12 +0900

    cfind関数を追加

--- menon/bits/find.hh
+++ menon/bits/find.hh
@@ -7,6 +7,10 @@
 
 namespace menon
 {
+  /// 列から値を探索する。
+  /// @param[in]  c     列コンテナ
+  /// @param[in]  value 探索する値
+  /// @return     cの要素にvalueに一致する要素があればその要素へのポインタを返す。なければnullptrを返す。
   template <typename C>
   inline auto find(C& c, typename C::value_type value)
     -> decltype(&*c.begin())
@@ -16,6 +20,25 @@ namespace menon
       return &*it;
     return nullptr;
   }
+
+  /// 列から値を探索する。
+  /// @param[in]  c     列コンテナ
+  /// @param[in]  value 探索する値
+  /// @return     cの要素にvalueに一致する要素があればその要素へのポインタを返す。なければnullptrを返す。
+  /// find関数との違いは、cfind関数はC::const_pointerを返すことである。
+  template <typename C>
+  inline auto cfind(C& c, typename C::value_type value)
+    -> typename C::const_pointer
+  {
+    auto it = std::find(c.cbegin(), c.cend(), value);
+    if (it != c.cend())
+      return &*it;
+    return nullptr;
+  }
+
+
+
+
 }
 
 #endif  // !MENON_BITS_FIND_HH_
--- test/test_find.cc
+++ test/test_find.cc
@@ -8,15 +8,28 @@ int main()
 {
   {
     std::vector v = { 3, 2, 7, 1, 9, 5 };
-    auto p = menon::find(v, 7);
-    BOOST_TEST_NE(p, nullptr);
-    if (p != nullptr)
     {
-      BOOST_TEST_EQ(*p, 7);
-      BOOST_TEST_EQ(p, v.data() + 2);
+      auto p = menon::find(v, 7);
+      BOOST_TEST_NE(p, nullptr);
+      if (p != nullptr)
+      {
+        BOOST_TEST_EQ(*p, 7);
+        BOOST_TEST_EQ(p, v.data() + 2);
+      }
+      p = menon::find(v, 6);
+      BOOST_TEST_EQ(p, nullptr);
     }
-    p = menon::find(v, 6);
-    BOOST_TEST_EQ(p, nullptr);
+    {
+      auto p = menon::cfind(v, 7);
+      BOOST_TEST_NE(p, nullptr);
+      if (p != nullptr)
+      {
+        BOOST_TEST_EQ(*p, 7);
+        BOOST_TEST_EQ(p, v.data() + 2);
+      }
+      p = menon::cfind(v, 6);
+      BOOST_TEST_EQ(p, nullptr);
+    }
   }
   {
     std::deque d = { 3, 2, 7, 1, 9, 5 };
