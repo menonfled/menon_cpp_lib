0f6261452b4d0f90202a2e0ebbbd9ef66aad45cd
menonfled <elddiry@gmail.com>
2020-09-16 07:39:55 +0900

    sync_object.hhからconfig.hhをインクルードするように変更

--- menon/bits/sync_object.hh
+++ menon/bits/sync_object.hh
@@ -1,10 +1,11 @@
-/// @file   menon/bits/sync_object.hh
+﻿/// @file   menon/bits/sync_object.hh
 /// @author @menonfled
 /// ライブラリ内で使用する同期オブジェクトの定義
 #ifndef MENON_BITS_SYNC_OBJECT_HH_
 #define MENON_BITS_SYNC_OBJECT_HH_
 #pragma once
 
+#include "menon/bits/config.hh"
 #include <mutex>
 
 namespace menon
