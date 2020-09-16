726459883173c6931da8f4dbcc9697d1677f8416
menonfled <elddiry@gmail.com>
2020-09-16 09:12:44 +0900

    コメント修正

--- menon/bits/mb_internal_encoding.hh
+++ menon/bits/mb_internal_encoding.hh
@@ -1,5 +1,6 @@
 ﻿/// @file   menon/bits/mb_internal_encoding.hh
 /// @author @menonfled
+/// mb_internal_encoding関数の定義
 #ifndef MENON_BITS_MB_INTERNAL_ENCODING_HH_
 #define MENON_BITS_MB_INTERNAL_ENCODING_HH_
 #pragma once
@@ -15,7 +16,8 @@ namespace menon
   /// encodingにnullptr以外を指定した場合、内部文字エンコーディングをその値に設定する。
   /// encodingにnullptrを指定した場合は設定は行わない。
   ///
-  /// この関数はPHPからの借用であるが、失敗時はfalseではなくnullptrを返す点に注意
+  /// この関数はPHPからの借用だが、成功時はtrueではなく以前に設定されていたエンコーディング文字列を、
+  /// 失敗時はfalseではなくnullptrを返す点に注意
   inline auto mb_internal_encoding(char const* encoding = nullptr)
     -> const char*
   {
