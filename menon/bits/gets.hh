760a5db4cbbd82a3879996800ecc009184943065
menonfled <elddiry@gmail.com>
2020-09-19 04:48:06 +0900

    gets関数を追加

---
+++ menon/bits/gets.hh
@@ -0,0 +1,76 @@
+﻿/// @file   menon/bits/gets.hh
+/// @author @menonfled
+/// gets関数の定義
+#ifndef MENON_BITS_GETS_HH_
+#define MENON_BITS_GETS_HH_
+#pragma once
+
+#include "menon/encoding.hh"
+#include <istream>
+#include <cstdio>
+#include <stdexcept>
+#include <string>
+
+namespace menon
+{
+  /// ストリームから1行読み込む
+  /// @param[in]  s       入力した文字列の格納先
+  /// @param[in]  n       配列sの要素数
+  /// @param[in]  stream  ストリーム
+  /// @return     sを返す。
+  /// 標準関数のgets関数と同様だが以下の点が異なる。
+  ///
+  /// - 標準入力以外のストリームを指定できる。
+  /// - 格納先配列の要素数を指定できる。
+  ///
+  /// 1行の長さがnより大きい場合は格納できない文字を捨てる。
+  template <typename Char>
+  auto gets(Char* s, int n, std::FILE* stream = stdin)
+    -> Char*
+  {
+    if (s == nullptr || n < 1 || stream == nullptr)
+      throw std::invalid_argument(__func__);
+
+    std::string buf;
+    buf.reserve(256);
+    {
+      flockfile(stream);
+      auto _ = gsl::finally([stream] {
+        funlockfile(stream);
+      });
+
+      while (!std::feof(stream))
+      {
+        auto c = getc_unlocked(stream);
+        if (c == EOF || c == Char('\n'))
+          break;
+        buf.push_back(char(c));
+      }
+    }
+    using ::menon::sv;
+    auto t = mb_convert_encoding<Char>(sv(buf), mb_external_encoding());
+    n = std::min(static_cast<int>(t.size()), n - 1);
+    t.copy(s, n);
+    s[n] = {};
+    return s;
+ }
+
+  /// ストリームから1行読み込む
+  /// @param[in]  s       入力した文字列の格納先
+  /// @param[in]  stream  ストリーム
+  /// @return     sを返す。
+  /// 標準関数のgets関数と同様だが以下の点が異なる。
+  ///
+  /// - 標準入力以外のストリームを指定できる。
+  /// - 格納先配列の要素数までしか書き込まれない。
+  ///
+  /// 1行の長さがnより大きい場合は格納できない文字を捨てる。
+  template <typename Char, std::size_t N>
+  inline auto gets(Char (&s)[N], std::FILE* stream = stdin)
+  {
+    return gets(s, N, stream);
+  }
+}
+
+#endif  // !MENON_BITS_GETS_HH_
+
---
+++ test/test_gets.cc
@@ -0,0 +1,31 @@
+﻿#include "menon/io.hh"
+#include <boost/core/lightweight_test.hpp>
+#include <fstream>
+
+void make_test_file(char const* path)
+{
+  std::ofstream ofs(path);
+  ofs << "これはテストです。" << std::endl;
+  ofs << "デフォルトの内部エンコーディングで文字列を表しています。" << std::endl;
+  ofs.close();
+  std::freopen(path, "r", stdin);
+}
+
+int main()
+{
+  auto path = "test.data";
+
+  make_test_file(path);
+  char buf[256] {};
+
+  BOOST_TEST_CSTR_EQ(menon::gets(buf, 256), "これはテストです。");
+  BOOST_TEST_CSTR_EQ(menon::gets(buf, 256), "デフォルトの内部エンコーディングで文字列を表しています。");
+  std::rewind(stdin);
+  BOOST_TEST_CSTR_EQ(menon::gets(buf), "これはテストです。");
+  BOOST_TEST_CSTR_EQ(menon::gets(buf), "デフォルトの内部エンコーディングで文字列を表しています。");
+
+  std::fclose(stdin);
+  std::remove(path);
+
+  return boost::report_errors();
+}
