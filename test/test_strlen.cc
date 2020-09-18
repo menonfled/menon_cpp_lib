d7860f9234448e4cb99fd2a69bf36b655717e558
menonfled <elddiry@gmail.com>
2020-09-19 01:57:36 +0900

    テストケースを追加

--- test/test_strlen.cc
+++ test/test_strlen.cc
@@ -3,8 +3,17 @@
 
 int main()
 {
+  using namespace std::literals;
+
   std::string s = "abcdefghi";
   std::size_t n = s.size();
+
+  constexpr auto r1 = menon::strlen("abcde");
+  BOOST_TEST_EQ(r1, 5);
+  constexpr auto r2 = menon::strlen("abcde", 3);
+  BOOST_TEST_EQ(r2, 3);
+  constexpr auto r3 = menon::strlen("abcde"sv);
+  BOOST_TEST_EQ(r3, 5);
 
   BOOST_TEST_EQ(menon::strlen(s), n);
   BOOST_TEST_EQ(menon::strlen(s.c_str()), n);
