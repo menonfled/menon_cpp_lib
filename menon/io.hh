fb4fc7852cad40e7218156b960d42b6a3d186621
menonfled <elddiry@gmail.com>
2020-09-19 04:48:45 +0900

    menon/io.hhを追加

---
+++ menon/io.hh
@@ -0,0 +1,10 @@
+﻿/// @file   menon/io.hh
+/// @author @menonfled
+/// 入出力のための宣言・定義
+#ifndef MENON_IO_HH_
+#define MENON_IO_HH_
+#pragma once
+
+#include "menon/bits/gets.hh"
+
+#endif  // !MENON_IO_HH_
