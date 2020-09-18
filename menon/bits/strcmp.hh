5f527ad38ef2df80bfe7fd77a7dfa7225b09647f
menonfled <elddiry@gmail.com>
2020-09-19 01:25:34 +0900

    strcasecmp関数を追加、constexprを除去

--- menon/bits/strcmp.hh
+++ menon/bits/strcmp.hh
@@ -6,21 +6,39 @@
 #pragma once
 
 #include "menon/bits/sv.hh"
+#include "menon/bits/toctrans.hh"
 
 namespace menon
 {
   namespace detail
   {
     template <typename Char, typename Traits>
-    constexpr auto strcmp_helper(std::basic_string_view<Char, Traits> sv1, std::basic_string_view<Char, Traits> sv2)
+    inline auto strcmp_helper(std::basic_string_view<Char, Traits> sv1, std::basic_string_view<Char, Traits> sv2)
     {
       return sv1.compare(sv2);
     }
 
     template <typename Char, typename Traits>
-    constexpr auto strncmp_helper(std::basic_string_view<Char, Traits> sv1, std::basic_string_view<Char, Traits> sv2, std::size_t n)
+    inline auto strncmp_helper(std::basic_string_view<Char, Traits> sv1, std::basic_string_view<Char, Traits> sv2, std::size_t n)
     {
       return sv1.compare(0, n, sv2, 0, n);
+    }
+
+    template <typename Char, typename Traits>
+    auto strcasecmp_helper(std::basic_string_view<Char, Traits> sv1, std::basic_string_view<Char, Traits> sv2)
+      -> int
+    {
+      auto ss1 = sv1.cbegin();
+      auto ss2 = sv2.cbegin();
+
+      while (ss1 != sv1.cend() && sv2.cend())
+      {
+        if (auto r = static_cast<int>(tolower(*ss1) - tolower(*ss2)))
+          return r;
+        ++ss1;
+        ++ss2;
+      }
+      return 0;
     }
   }
 
@@ -29,7 +47,7 @@ namespace menon
   /// @param[in]  s2  比較対象の文字列
   /// @retuen     s1 < s2のときは負、s1 == s2のときは0、s1 > s2のときは正の値を返す。
   template <typename T, typename U>
-  constexpr auto strcmp(T const& s1, U const& s2)
+  inline auto strcmp(T const& s1, U const& s2)
   {
     using menon::sv;
     return detail::strcmp_helper(sv(s1), sv(s2));
@@ -41,10 +59,21 @@ namespace menon
   /// @param[in]  n   指定文字数
   /// @retuen     s1 < s2のときは負、s1 == s2のときは0、s1 > s2のときは正の値を返す。
   template <typename T, typename U>
-  constexpr auto strncmp(T const& s1, U const& s2, std::size_t n)
+  inline auto strncmp(T const& s1, U const& s2, std::size_t n)
   {
     using menon::sv;
     return detail::strncmp_helper(sv(s1), sv(s2), n);
+  }
+
+  /// 大文字小文字を区別しない文字列の比較
+  /// @param[in]  s1  比較対象の文字列
+  /// @param[in]  s2  比較対象の文字列
+  /// @retuen     s1 < s2のときは負、s1 == s2のときは0、s1 > s2のときは正の値を返す。
+  template <typename T, typename U>
+  inline auto strcasecmp(T const& s1, U const& s2)
+  {
+    using menon::sv;
+    return detail::strcasecmp_helper(sv(s1), sv(s2));
   }
 }
 
--- test/test_strcmp.cc
+++ test/test_strcmp.cc
@@ -15,6 +15,13 @@ int main()
     BOOST_TEST(menon::strncmp(s, "abcrtylk", 3) == 0);
     BOOST_TEST(menon::strncmp(s, "fgnrk", 5) < 0);
     BOOST_TEST(menon::strncmp(s, "aaekj", 5) > 0);
+
+    BOOST_TEST(menon::strcasecmp(s, sv) == 0);
+    BOOST_TEST(menon::strcasecmp(s, "fgnrk") < 0);
+    BOOST_TEST(menon::strcasecmp(s, "aaekj") > 0);
+    BOOST_TEST(menon::strcasecmp(s, "AbcDefGhi") == 0);
+    BOOST_TEST(menon::strcasecmp(s, "FgNrk") < 0);
+    BOOST_TEST(menon::strcasecmp(s, "aAeKJ") > 0);
   }
   {
     std::wstring s = L"abcdefghi";
@@ -28,6 +35,13 @@ int main()
     BOOST_TEST(menon::strncmp(s, L"abcrtylk", 3) == 0);
     BOOST_TEST(menon::strncmp(s, L"fgnrk", 5) < 0);
     BOOST_TEST(menon::strncmp(s, L"aaekj", 5) > 0);
+
+    BOOST_TEST(menon::strcasecmp(s, sv) == 0);
+    BOOST_TEST(menon::strcasecmp(s, L"fgnrk") < 0);
+    BOOST_TEST(menon::strcasecmp(s, L"aaekj") > 0);
+    BOOST_TEST(menon::strcasecmp(s, L"AbcDefGhi") == 0);
+    BOOST_TEST(menon::strcasecmp(s, L"FgNrk") < 0);
+    BOOST_TEST(menon::strcasecmp(s, L"aAeKJ") > 0);
   }
   return boost::report_errors();
 }
