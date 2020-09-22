ec9515310bc7dadd78104d11158f6778732470dd
menonfled <elddiry@gmail.com>
2020-09-22 15:32:47 +0900

    strncat関数を追加

--- menon/bits/strcat.hh
+++ menon/bits/strcat.hh
@@ -69,6 +69,53 @@ namespace menon
     static_assert(std::is_same_v<Char, typename decltype(ss2)::value_type>);
     return s1.append(ss2.cbegin(), ss2.cend());
   }
+
+  /// 指定文字数だけ文字列を連結
+  /// @param[in,out]  s1    先行部分の文字列および結果の格納先
+  /// @param[in]      n1    s1が指す配列の要素数
+  /// @param[in]      s2    連結するの文字列
+  /// @param[in]      n2    指定文字数
+  /// @return         s1を返す。
+  /// s1に格納されている文字列にs2が指す文字列を連結し、結果をs1に格納する。
+  /// nが1要素以上ある場合、s1には必ずナル文字を格納する。
+  /// @attention  文字列が切り詰められることで予期しない結果になる場合があるので要注意。  
+  template <typename Char, typename String>
+  auto strncat(Char* s1, std::size_t n1, String const& s2, std::size_t n2)
+  {
+    using ::menon::sv;
+    return detail::strcat_helper(s1, n1, sv(s2).substr(0, n2));
+  }
+
+  /// 指定文字数だけ文字列を連結
+  /// @param[in,out]  s1    先行部分の文字列および結果の格納先
+  /// @param[in]      s2    連結するの文字列
+  /// @param[in]      n     指定文字数
+  /// @return         s1を返す。
+  /// s1に格納されている文字列にs2が指す文字列を連結し、結果をs1に格納する。
+  /// nが1要素以上ある場合、s1には必ずナル文字を格納する。
+  /// @attention  文字列が切り詰められることで予期しない結果になる場合があるので要注意。  
+  template <typename Char, std::size_t N, typename String>
+  auto strncat(Char (&s1)[N], String const& s2, std::size_t n)
+  {
+    using ::menon::sv;
+    return detail::strcat_helper(s1, N, sv(s2).substr(0, n));
+  }
+
+  /// 指定文字数だけ文字列を連結
+  /// @param[in,out]  s1    先行部分の文字列および結果の格納先
+  /// @param[in]      s2    連結するの文字列
+  /// @param[in]      n     指定文字数
+  /// @return         s1を返す。
+  /// s1に格納されている文字列にs2が指す文字列を連結し、結果をs1に格納する。
+  template <typename Char, typename Traits, typename Allocator, typename String>
+  auto strncat(std::basic_string<Char, Traits, Allocator>& s1, String const& s2, std::size_t n)
+    -> std::basic_string<Char, Traits, Allocator>&
+  {
+    using ::menon::sv;
+    auto ss2 = sv(s2).substr(0, n);
+    static_assert(std::is_same_v<Char, typename decltype(ss2)::value_type>);
+    return s1.append(ss2.cbegin(), ss2.cend());
+  }
 }
 
 #endif  // !MENON_BITS_STRCAT_HH_
--- test/test_strcat.cc
+++ test/test_strcat.cc
@@ -33,5 +33,45 @@ int main()
     std::wstring wstr = L"abc";
     BOOST_TEST_EQ(std::wcscmp(menon::strcat(wstr, L"def").c_str(), L"abcdef"), 0);
   }
+
+  {
+    char s[16] = "abc";
+    BOOST_TEST_CSTR_EQ(menon::strncat(s, 16, "defghi", 3), "abcdef");
+  }
+  {
+    char s[16] = "abc";
+    BOOST_TEST_CSTR_EQ(menon::strncat(s, 16, "def", 5), "abcdef");
+  }
+  {
+    char s[16] = "abc";
+    BOOST_TEST_CSTR_EQ(menon::strncat(s, "defghi", 3), "abcdef");
+  }
+  {
+    char s[16] = "abc";
+    BOOST_TEST_CSTR_EQ(menon::strncat(s, "def", 5), "abcdef");
+  }
+  {
+    wchar_t wcs[16] = L"abc";
+    BOOST_TEST_EQ(std::wcscmp(menon::strncat(wcs, 16, L"defghi", 3), L"abcdef"), 0);
+  }
+  {
+    wchar_t wcs[16] = L"abc";
+    BOOST_TEST_EQ(std::wcscmp(menon::strncat(wcs, L"defghi", 3), L"abcdef"), 0);
+  }
+  {
+    wchar_t wcs[16] = L"abc";
+    BOOST_TEST_EQ(std::wcscmp(menon::strncat(wcs, 16, L"def", 5), L"abcdef"), 0);
+  }
+  {
+    wchar_t wcs[16] = L"abc";
+    BOOST_TEST_EQ(std::wcscmp(menon::strncat(wcs, L"def", 5), L"abcdef"), 0);
+  }
+  {
+    std::string str = "abc";
+    BOOST_TEST_EQ(menon::strncat(str, "defghi", 3), "abcdef");
+
+    std::wstring wstr = L"abc";
+    BOOST_TEST_EQ(std::wcscmp(menon::strncat(wstr, L"defghi", 3).c_str(), L"abcdef"), 0);
+  }
   return boost::report_errors();
 }
