bbb59a2c86464fe5e1738f1c9fddf5a7516dcf8b
menonfled <elddiry@gmail.com>
2020-09-23 23:22:40 +0900

    implode関数のテンプレート仮引数を名称変更

--- menon/bits/implode.hh
+++ menon/bits/implode.hh
@@ -35,8 +35,8 @@ namespace menon
   /// @param[in]  c     文字列の列
   /// @return     連結した結果をstringとして返す。
   /// この関数はPHPのimplodeからの借用である。
-  template <typename T, typename U>
-  auto implode(T const& glue, U const& c)
+  template <typename String1, typename String2>
+  auto implode(String1 const& glue, String2 const& c)
   {
     using ::std::begin;
     using ::std::end;
@@ -48,8 +48,8 @@ namespace menon
   /// @param[in]  glue  区切り文字
   /// @param[in]  il    文字列の列
   /// @return     連結した結果をstringとして返す。
-  template <typename T, typename U>
-  auto implode(T const& glue, std::initializer_list<U> il)
+  template <typename String1, typename String2>
+  auto implode(String1 const& glue, std::initializer_list<String2> il)
   {
     using ::menon::sv;
     return detail::implode_helper(sv(glue), il.begin(), il.end());
@@ -60,8 +60,8 @@ namespace menon
   /// @param[in]  first 文字列の列の先頭を指す反復子
   /// @param[in]  last  文字列の列の末尾の次を指す反復子
   /// @return     連結した結果をstringとして返す。
-  template <typename T, typename InputIterator>
-  auto implode(T const& glue, InputIterator first, InputIterator last)
+  template <typename String, typename InputIterator>
+  auto implode(String const& glue, InputIterator first, InputIterator last)
   {
     using ::menon::sv;
     return detail::implode_helper(sv(glue), first, last);
