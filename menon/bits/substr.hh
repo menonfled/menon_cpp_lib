1ed1866c3eec99dd64b3fa7d1a61c52bb9fb0115
menonfled <elddiry@gmail.com>
2020-09-25 03:22:16 +0900

    substr関数の関数ヘッダーを追加

--- menon/bits/substr.hh
+++ menon/bits/substr.hh
@@ -9,6 +9,13 @@
 
 namespace menon
 {
+  /// 部分文字列の取得
+  /// @param[in]  s     元の文字列
+  /// @param[in]  pos   部分文字列の開始位置
+  /// @param[in]  n     部分文字列の長さ
+  /// @return     sのうち区間[pos, pos+n)にあたる部分文字列を返す。
+  /// @throw      posにsの範囲外の値を指定した場合はout_of_range例外を送出する。
+  /// @attention  pos+nがsの長さより大きい場合はsの長さまで切り詰める。
   template <typename String>
   constexpr auto substr(String const& s, std::size_t pos, std::size_t n = SIZE_MAX)
   {
