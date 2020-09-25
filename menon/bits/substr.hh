aa99a06e97397cb2d188387860280702b59e33fb
menonfled <elddiry@gmail.com>
2020-09-25 20:20:54 +0900

    leftおよびright関数を追加

--- menon/bits/substr.hh
+++ menon/bits/substr.hh
@@ -22,6 +22,34 @@ namespace menon
     using ::menon::sv;
     return sv(s).substr(pos, n);
   }
+
+  /// 左端部分文字列の取得
+  /// @param[in]  s     元の文字列
+  /// @param[in]  n     部分文字列の長さ
+  /// @return     sのうち区間[0, pos+n)にあたる部分文字列を返す。
+  /// @attention  pos+nがsの長さより大きい場合はsの長さまで切り詰める。
+  template <typename String>
+  constexpr auto left(String const& s, std::size_t n)
+  {
+    return substr(s, 0, n);
+  }
+
+  /// 右端左端部分文字列の取得
+  /// @param[in]  s     元の文字列
+  /// @param[in]  n     部分文字列の長さ
+  /// @return     sのうち区間[s.size()-n, s.size())にあたる部分文字列を返す。
+  /// @attention  nがsの長さより大きい場合はsを返す。
+  template <typename String>
+  constexpr auto right(String const& s, std::size_t n)
+  {
+    using ::menon::sv;
+    auto t = sv(s);
+    auto length = t.size();
+    if (n > length)
+      n = length;
+    auto pos = length - n;
+    return t.substr(pos, n);
+  }
 }
 
 #endif  // !MENON_BITS_SUBSTR_HH_
