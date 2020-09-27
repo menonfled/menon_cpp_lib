def8dd13879486ce54af9e38f33bacc578e6bf04
menonfled <elddiry@gmail.com>
2020-09-27 15:19:34 +0900

    浮動小数点数版のdiv関数を追加

--- menon/bits/div.hh
+++ menon/bits/div.hh
@@ -21,6 +21,12 @@ namespace menon
     T rem;  ///< 剰余
   };
 
+  /// 整数除算における商と剰余
+  /// @param[in]  numer   被除数
+  /// @param[in]  denom   除数
+  /// @return     numer÷denomの商と剰余をdiv_t型で返す。
+  /// @throw      remが0の場合、invalid_argument例外を送出する。
+  /// @throw      quot/remが符号付き整数型であり、quotが型の最小値かつremが-1の場合、out_of_range例外を送出する。
   template <std::integral T, std::integral U>
   constexpr auto div(T numer, U denom)
   {
@@ -31,6 +37,23 @@ namespace menon
       if (numer == std::numeric_limits<value_type>::min() && denom == -1)
         throw std::out_of_range("menon::div");
     div_t<value_type> r { numer / denom, numer % denom };
+    return r;
+  }
+
+  /// 浮動小数点除算における商と剰余
+  /// @param[in]  numer   被除数
+  /// @param[in]  denom   除数
+  /// @return     numer÷denomの商と剰余をdiv_t型で返す。
+  /// @throw      remが0の場合、invalid_argument例外を送出する。
+  template <typename T, typename U>
+  auto div(T numer, U denom)
+  {
+    using value_type = decltype(numer / denom);
+    if (denom == 0 && numer != 0)
+      throw std::invalid_argument("menon::div");
+    div_t<value_type> r;
+    std::modf(numer / denom, &r.quot);
+    r.rem = std::fmod(numer, denom);
     return r;
   }
 }
--- test/test_div.cc
+++ test/test_div.cc
@@ -12,12 +12,28 @@ int main()
     BOOST_TEST_THROWS(menon::div(INT_MIN, -1), std::out_of_range);
   }
   {
+    constexpr auto d = menon::div(12345u, 67);
+    BOOST_TEST_EQ(d.quot, 12345u / 67);
+    BOOST_TEST_EQ(d.rem, 12345u % 67);
+    BOOST_TEST_THROWS(menon::div(123u, 0), std::invalid_argument);
+    BOOST_TEST_EQ(menon::div(INT_MIN, -1u).quot, INT_MIN / (-1u));
+  }
+  {
     constexpr auto d = menon::div(12345LL, 67);
     BOOST_TEST_EQ(d.quot, 12345LL / 67);
     BOOST_TEST_EQ(d.rem, 12345LL % 67);
     BOOST_TEST_THROWS(menon::div(123, 0LL), std::invalid_argument);
     BOOST_TEST_THROWS(menon::div(LONG_LONG_MIN, -1), std::out_of_range);
     BOOST_TEST_EQ(menon::div(INT_MIN, -1LL).quot, INT_MIN / -1LL);
+  }
+  {
+    auto d = menon::div(12345.0, 67);
+    BOOST_TEST_EQ(d.quot, 12345 / 67);
+    BOOST_TEST_EQ(d.rem, 12345 % 67);
+    BOOST_TEST_THROWS(menon::div(123.0, 0), std::invalid_argument);
+    BOOST_TEST_EQ(menon::div(INT_MIN, -1.0).quot, INT_MIN * -1.0);
+    BOOST_TEST(std::isnan(menon::div(INFINITY, INFINITY).quot));
+    BOOST_TEST(std::isnan(menon::div(0.0, 0.0).quot));
   }
   return boost::report_errors();
 }
