942c4a3440c86833ceb6072a6ab5e4beabb964e6
menonfled <elddiry@gmail.com>
2020-09-28 00:12:23 +0900

    menon/bits/alnum.hhを追加

---
+++ menon/bits/alnum.hh
@@ -0,0 +1,14 @@
+﻿/// @file   menon/bits/alnum.hh
+/// 36進数用英数字配列の定義
+#ifndef MENON_BITS_ALNUM_HH_
+#define MENON_BITS_ALNUM_HH_
+#pragma once
+
+#include "menon/bits/config.hh"
+
+namespace menon
+{
+  constexpr char const alnum[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+}
+
+#endif  // !MENON_BITS_ALNUM_HH_
