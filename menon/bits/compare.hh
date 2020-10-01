05835ee0365919c621f627657d3f1cf2bd8466a6
menonfled <elddiry@gmail.com>
2020-10-02 00:02:39 +0900

    lt関数の浮動小数点数の引数は両辺が同じ型の場合のみ有効になるように修正

--- menon/bits/compare.hh
+++ menon/bits/compare.hh
@@ -13,7 +13,7 @@ namespace menon
 {
   namespace detail
   {
-    template <typename T, typename U>
+    template <std::integral T, std::integral U>
     constexpr bool lt_helper(T lhs, U rhs)
     {
       return lhs < rhs;
@@ -38,14 +38,24 @@ namespace menon
     }
   }
 
-  /// 算術型の比較（左辺 < 右辺）
+  /// 整数の比較（左辺 < 右辺）
   /// @param[in]  lhs     左辺
   /// @param[in]  rhs     右辺
   /// lhs < rhsの場合はtrueを、それ以外はfalseを返す。
-  template <typename T, typename U> requires std::is_arithmetic_v<T> && std::is_arithmetic_v<U>
+  template <std::integral T, std::integral U>
   constexpr bool lt(T lhs, U rhs)
   {
     return detail::lt_helper(+lhs, +rhs);
+  }
+
+  /// 浮動小数点数の比較（左辺 < 右辺）
+  /// @param[in]  lhs     左辺
+  /// @param[in]  rhs     右辺
+  /// lhs < rhsの場合はtrueを、それ以外はfalseを返す。
+  template <std::floating_point T>
+  constexpr bool lt(T lhs, T rhs)
+  {
+    return lhs < rhs;
   }
 }
 
