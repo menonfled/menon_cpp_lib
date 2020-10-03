1efbe3149a3ed8b0a01e6ee4c2f693fa8a83b141
menonfled <elddiry@gmail.com>
2020-10-03 14:39:15 +0900

    gt, le, およびge関数のテストコードを追加

--- test/test_compare.cc
+++ test/test_compare.cc
@@ -13,5 +13,40 @@ int main()
   BOOST_TEST(menon::lt(-2, 1u));
   BOOST_TEST(menon::lt(123.45, 345.67));
   BOOST_TEST(!menon::lt(123.45, 123.45));
+
+  BOOST_TEST(!menon::gt(1, 2));
+  BOOST_TEST(menon::gt(2, 1));
+  BOOST_TEST(!menon::gt(2, 2));
+  BOOST_TEST(!menon::gt(1L, 2));
+  BOOST_TEST(menon::gt(2, 1L));
+  BOOST_TEST(!menon::gt(2, 2LL));
+  BOOST_TEST(menon::gt(2, -1));
+  BOOST_TEST(!menon::gt(-2, 1));
+  BOOST_TEST(!menon::gt(-2, 1u));
+  BOOST_TEST(!menon::gt(123.45, 345.67));
+  BOOST_TEST(!menon::gt(123.45, 123.45));
+
+  BOOST_TEST(menon::le(1, 2));
+  BOOST_TEST(!menon::le(2, 1));
+  BOOST_TEST(menon::le(2, 2));
+  BOOST_TEST(menon::le(1L, 2));
+  BOOST_TEST(!menon::le(2, 1L));
+  BOOST_TEST(menon::le(2, 2LL));
+  BOOST_TEST(menon::le(-2, 1));
+  BOOST_TEST(menon::le(-2, 1u));
+  BOOST_TEST(menon::le(123.45, 345.67));
+  BOOST_TEST(menon::le(123.45, 123.45));
+
+  BOOST_TEST(!menon::ge(1, 2));
+  BOOST_TEST(menon::ge(2, 1));
+  BOOST_TEST(menon::ge(2, 2));
+  BOOST_TEST(!menon::ge(1L, 2));
+  BOOST_TEST(menon::ge(2, 1L));
+  BOOST_TEST(menon::ge(2, 2LL));
+  BOOST_TEST(!menon::ge(-2, 1));
+  BOOST_TEST(!menon::ge(-2, 1u));
+  BOOST_TEST(!menon::ge(123.45, 345.67));
+  BOOST_TEST(menon::ge(123.45, 123.45));
+
   return boost::report_errors();
 }
