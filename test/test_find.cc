928a4e3271353f4388c964b63c7552aaa3d1c5ae
menonfled <elddiry@gmail.com>
2020-10-23 00:38:20 +0900

    test_find.ccにおいてnullptrの比較を排除

--- test/test_find.cc
+++ test/test_find.cc
@@ -13,117 +13,117 @@ int main()
     std::vector v = { 3, 2, 7, 1, 9, 5 };
     {
       auto p = menon::find(v, 7);
-      BOOST_TEST_NE(p, nullptr);
-      if (p != nullptr)
+      BOOST_TEST(p);
+      if (p)
       {
         BOOST_TEST_EQ(*p, 7);
         BOOST_TEST_EQ(p, v.data() + 2);
       }
       p = menon::find(v, 6);
-      BOOST_TEST_EQ(p, nullptr);
+      BOOST_TEST_NOT(p);
     }
     {
       auto p = menon::cfind(v, 7);
-      BOOST_TEST_NE(p, nullptr);
-      if (p != nullptr)
+      BOOST_TEST(p);
+      if (p)
       {
         BOOST_TEST_EQ(*p, 7);
         BOOST_TEST_EQ(p, v.data() + 2);
       }
       p = menon::cfind(v, 6);
-      BOOST_TEST_EQ(p, nullptr);
+      BOOST_TEST_NOT(p);
     }
   }
   {
     std::deque d = { 3, 2, 7, 1, 9, 5 };
     auto p = menon::find(d, 7);
-    BOOST_TEST_NE(p, nullptr);
-    if (p != nullptr)
+    BOOST_TEST(p);
+    if (p)
     {
       BOOST_TEST_EQ(*p, 7);
     }
     p = menon::find(d, 6);
-    BOOST_TEST_EQ(p, nullptr);
+    BOOST_TEST_NOT(p);
   }
   {
     std::list l = { 3, 2, 7, 1, 9, 5 };
     auto p = menon::find(l, 7);
-    BOOST_TEST_NE(p, nullptr);
-    if (p != nullptr)
+    BOOST_TEST(p);
+    if (p)
     {
       BOOST_TEST_EQ(*p, 7);
     }
     p = menon::find(l, 6);
-    BOOST_TEST_EQ(p, nullptr);
+    BOOST_TEST_NOT(p);
   }
   int a[] = { 3, 2, 7, 1, 9, 5 };
   {
     auto p = menon::find(a, 7);
-    BOOST_TEST_NE(p, nullptr);
-    if (p != nullptr)
+    BOOST_TEST(p);
+    if (p)
     {
       BOOST_TEST_EQ(*p, 7);
       BOOST_TEST_EQ(p, a + 2);
     }
     p = menon::find(a, 6);
-    BOOST_TEST_EQ(p, nullptr);
+    BOOST_TEST_NOT(p);
   }
   {
     auto p = menon::cfind(a, 7);
-    BOOST_TEST_NE(p, nullptr);
-    if (p != nullptr)
+    BOOST_TEST(p);
+    if (p)
     {
       BOOST_TEST_EQ(*p, 7);
       BOOST_TEST_EQ(p, a + 2);
     }
     p = menon::cfind(a, 6);
-    BOOST_TEST_EQ(p, nullptr);
+    BOOST_TEST_NOT(p);
   }
 
   {
     std::set s = { 3, 2, 7, 1, 9, 5 };
     {
       auto p = menon::find(s, 7);
-      BOOST_TEST_NE(p, nullptr);
-      if (p != nullptr)
+      BOOST_TEST(p);
+      if (p)
       {
         BOOST_TEST_EQ(*p, 7);
       }
       p = menon::find(s, 6);
-      BOOST_TEST_EQ(p, nullptr);
+      BOOST_TEST_NOT(p);
     }
     {
       auto p = menon::cfind(s, 7);
-      BOOST_TEST_NE(p, nullptr);
-      if (p != nullptr)
+      BOOST_TEST(p);
+      if (p)
       {
         BOOST_TEST_EQ(*p, 7);
       }
       p = menon::cfind(s, 6);
-      BOOST_TEST_EQ(p, nullptr);
+      BOOST_TEST_NOT(p);
     }
   }
 
   std::map<std::string, int> m = { { "d", 3 }, { "y", 2 }, { "c", 7 }, { "v", 1 } };
   {
     auto p = menon::find(m, "c");
-    BOOST_TEST_NE(p, nullptr);
-    if (p != nullptr)
+    BOOST_TEST(p);
+    if (p)
     {
       BOOST_TEST_EQ(*p, 7);
     }
     p = menon::find(m, "k");
-    BOOST_TEST_EQ(p, nullptr);
+    BOOST_TEST_NOT(p);
   }
   {
     auto p = menon::cfind(m, "c");
-    BOOST_TEST_NE(p, nullptr);
-    if (p != nullptr)
+    BOOST_TEST(p);
+    if (p)
     {
       BOOST_TEST_EQ(*p, 7);
     }
     p = menon::cfind(m, "k");
-    BOOST_TEST_EQ(p, nullptr);
+    BOOST_TEST_NOT(p);
   }
   return boost::report_errors();
 }
