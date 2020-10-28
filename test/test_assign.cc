632173f85cebbb3977463262ea568dac3911ece1
menonfled <elddiry@gmail.com>
2020-10-28 18:11:24 +0900

    assign関数のテストコードを追加

--- test/test_assign.cc
+++ test/test_assign.cc
@@ -1,18 +1,25 @@
-﻿#include "menon/utility.hh"
+﻿// test_assign.cc
+#include "menon/utility.hh"
 #include <boost/core/lightweight_test.hpp>
 #include <vector>
 #include <list>
+#include <deque>
 
 int main()
 {
   std::vector<int> v{ 1, 2, 3 };
-  {
+  { // vectorに対するテストコード
     std::vector<int> t;
     menon::assign(v, t);
     BOOST_TEST_ALL_EQ(v.cbegin(), v.cend(), t.cbegin(), t.cend());
   }
-  {
+  { // listに対するテストコード
     std::list<int> t;
+    menon::assign(v, t);
+    BOOST_TEST_ALL_EQ(v.cbegin(), v.cend(), t.cbegin(), t.cend());
+  }
+  { // dequeに対するテストコード
+    std::deque<int> t;
     menon::assign(v, t);
     BOOST_TEST_ALL_EQ(v.cbegin(), v.cend(), t.cbegin(), t.cend());
   }
