3f69372c25dadc2f4f49ef7d4898495145ba639e
menonfled <elddiry@gmail.com>
2020-10-28 22:23:01 +0900

    c8_to_c32関数のテストコードを改善

--- test/test_c8_to_c32.cc
+++ test/test_c8_to_c32.cc
@@ -1,9 +1,11 @@
+// test_c8_to_c32.c
 #include "menon/encoding.hh"
 #include <boost/core/lightweight_test.hpp>
 #include <algorithm>
 #include <iconv.h>
 #include <cstdio>
 
+// UTF-32の1文字についてのテストコード
 void test(char32_t c32)
 {
   union
@@ -23,7 +25,7 @@ void test(char32_t c32)
 
   utf32 = c32;
 
-  // std::fprintf(stderr, "c32 = U+%08x\n", c32);
+  // UTF-32LEからUTF-8に変換し、比較対象のデータを作る。
   if (auto cd = iconv_open("utf-8", "utf-32LE"))
   {
     iconv(cd, &p_inbuf, &inbyteleft, &p_outbuf, &outbyteleft);
@@ -34,14 +36,18 @@ void test(char32_t c32)
     std::perror("fail:");
   }
 
+  // UTF-8からUTF-32へ変換する。
   auto n = menon::c8_to_c32(utf8, 4, &utf32);
 
+  // c8_to_c32関数は1以上を返すはず
   BOOST_TEST_GT(n, 0);
+  // c8_to_c32関数で変換した結果とiconvで得た比較対象のUTF-8は等しいはず
   BOOST_TEST_EQ((int)utf32, (int)c32);
 }
 
 int main()
 {
+  // 全種類の文字を総当たりする。
   for (char32_t c32 = 0; c32 <= 0x0ffff; c32++)
   {
     if ((0xd800 <= c32 && c32 <= 0xdbff) || (0xdc00 <= c32 && c32 <= 0xdfff))
