bd3acce1f2ed88d786db4e5dffe10b4f0cfc9d06
menonfled <elddiry@gmail.com>
2020-10-30 16:59:35 +0900

    比較関数のテストコードを改善

--- test/test_compare.cc
+++ test/test_compare.cc
@@ -1,7 +1,9 @@
-﻿#include "menon/utility.hh"
+﻿// test_compare.cc
+#include "menon/utility.hh"
 #include <boost/core/lightweight_test.hpp>
 
-int main()
+// lt関数のテストコード
+void test_lt()
 {
   BOOST_TEST(menon::lt(1, 2));
   BOOST_TEST(!menon::lt(2, 1));
@@ -13,7 +15,11 @@ int main()
   BOOST_TEST(menon::lt(-2, 1u));
   BOOST_TEST(menon::lt(123.45, 345.67));
   BOOST_TEST(!menon::lt(123.45, 123.45));
+}
 
+// gt関数のテストコード
+void test_gt()
+{
   BOOST_TEST(!menon::gt(1, 2));
   BOOST_TEST(menon::gt(2, 1));
   BOOST_TEST(!menon::gt(2, 2));
@@ -25,7 +31,11 @@ int main()
   BOOST_TEST(!menon::gt(-2, 1u));
   BOOST_TEST(!menon::gt(123.45, 345.67));
   BOOST_TEST(!menon::gt(123.45, 123.45));
+}
 
+// le関数のテストコード
+void test_le()
+{
   BOOST_TEST(menon::le(1, 2));
   BOOST_TEST(!menon::le(2, 1));
   BOOST_TEST(menon::le(2, 2));
@@ -36,7 +46,11 @@ int main()
   BOOST_TEST(menon::le(-2, 1u));
   BOOST_TEST(menon::le(123.45, 345.67));
   BOOST_TEST(menon::le(123.45, 123.45));
+}
 
+// ge関数のテストコード
+void test_ge()
+{
   BOOST_TEST(!menon::ge(1, 2));
   BOOST_TEST(menon::ge(2, 1));
   BOOST_TEST(menon::ge(2, 2));
@@ -47,7 +61,11 @@ int main()
   BOOST_TEST(!menon::ge(-2, 1u));
   BOOST_TEST(!menon::ge(123.45, 345.67));
   BOOST_TEST(menon::ge(123.45, 123.45));
+}
 
+// eq関数のテストコード
+void test_eq()
+{
   BOOST_TEST(!menon::eq(1, 2));
   BOOST_TEST(!menon::eq(2, 1));
   BOOST_TEST(menon::eq(2, 2));
@@ -58,7 +76,11 @@ int main()
   BOOST_TEST(!menon::eq(-2, 1u));
   BOOST_TEST(!menon::eq(123.45, 345.67));
   BOOST_TEST(menon::eq(123.45, 123.45));
+}
 
+// ne関数のテストコード
+void test_ne()
+{
   BOOST_TEST(menon::ne(1, 2));
   BOOST_TEST(menon::ne(2, 1));
   BOOST_TEST(!menon::ne(2, 2));
@@ -70,6 +92,15 @@ int main()
   BOOST_TEST(menon::ne(-1, ~0u));
   BOOST_TEST(menon::ne(123.45, 345.67));
   BOOST_TEST(!menon::ne(123.45, 123.45));
+}
 
+int main()
+{
+  test_lt();
+  test_gt();
+  test_le();
+  test_ge();
+  test_eq();
+  test_ne();
   return boost::report_errors();
 }
