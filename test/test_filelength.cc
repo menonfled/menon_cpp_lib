9fbf077024ec6ef3cf4ed0c9b36013e104c94ad0
menonfled <elddiry@gmail.com>
2020-11-02 17:25:16 +0900

    test_filelength.ccにコメントを追加

--- test/test_filelength.cc
+++ test/test_filelength.cc
@@ -1,4 +1,5 @@
-﻿#include "menon/io.hh"
+﻿// test_filelength.cc
+#include "menon/io.hh"
 #include <boost/core/lightweight_test.hpp>
 #include <cstddef>
 #include <vector>
@@ -9,6 +10,7 @@
 # include <io.h>
 #endif
 
+// テスト用のファイルを作成
 long long make_test_file(char const* path)
 {
   std::vector<std::byte> v(256, std::byte(123));
@@ -25,21 +27,25 @@ int main()
   char const* path = "test.data";
   auto n = make_test_file(path);
 
+  // ファイルディスクリプタからファイルサイズを取得
   int fd = open(path, O_RDONLY | O_BINARY);
   BOOST_TEST_GE(fd, 0);
   BOOST_TEST_EQ(menon::filelength(fd), n);
   close(fd);
 
+  // FILE*からファイルサイズを取得
   auto stream = fopen(path, "r");
   BOOST_TEST_NE(stream, nullptr);
   BOOST_TEST_EQ(menon::filelength(stream), n);
   fclose(stream);
 
+  // filesystem::pathからファイルサイズを取得
   std::filesystem::path t(path);
   BOOST_TEST_EQ(menon::filelength(t), n);
   BOOST_TEST_EQ(menon::filesize(t), n);
   BOOST_TEST_EQ(menon::filesize(path), n);
 
+  // 不要になったテスト用のファイルを削除
   std::remove(path);
   return boost::report_errors();
 }
