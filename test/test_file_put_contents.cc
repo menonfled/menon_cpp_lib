ab2cbe3e44982f9c9ff3f5be658ebd14858a2bf2
menonfled <elddiry@gmail.com>
2020-11-02 15:16:50 +0900

    test_file_put_contents.ccにコメントを追加

--- test/test_file_put_contents.cc
+++ test/test_file_put_contents.cc
@@ -1,4 +1,5 @@
-﻿#include "menon/io.hh"
+﻿// test_file_put_contents.cc
+#include "menon/io.hh"
 #include <boost/core/lightweight_test.hpp>
 #include <cstddef>
 #include <cstring>
@@ -8,6 +9,7 @@
 
 std::byte test_data[256];
 
+// テストコードが書き出したテストデータの読み込み
 std::vector<std::byte> get_test_file(char const* path)
 {
   auto stream = std::fopen(path, "rb");
@@ -22,11 +24,12 @@ std::vector<std::byte> get_test_file(char const* path)
   return r;
 }
 
+// stream_put_contents関数のテストコード
 void test_stream_put_contents(char const* path)
 {
   auto n = sizeof(test_data);
 
-  {
+  { // FILE*版の基本的な使い方
     auto stream = std::fopen(path, "wb");
     BOOST_TEST_NE(stream, nullptr);
     BOOST_TEST_EQ(menon::stream_put_contents(stream, test_data), n);
@@ -36,10 +39,11 @@ void test_stream_put_contents(char const* path)
     BOOST_TEST_EQ(std::memcmp(data.data(), test_data, n), 0);
   }
   {
-    {
+    { // ostream版の基本的な使い方
       std::ofstream ofs(path, std::ios_base::out | std::ios_base::binary);
       BOOST_TEST(ofs.is_open());
       BOOST_TEST_EQ(menon::stream_put_contents(ofs, std::span<std::byte>(test_data)), n);
+      // ここでofsをclose
     }
     auto data = get_test_file(path);
     BOOST_TEST_EQ(data.size(), n);
@@ -47,24 +51,25 @@ void test_stream_put_contents(char const* path)
   }
 }
 
+// file_put_contents関数のテストコード
 void test_file_put_contents(char const* path)
 {
   auto n = sizeof(test_data);
 
-  {
+  { // 配列の書き出し
     BOOST_TEST_EQ(menon::file_put_contents(path, test_data), n);
     auto data = get_test_file(path);
     BOOST_TEST_EQ(data.size(), n);
     BOOST_TEST_EQ(std::memcmp(data.data(), test_data, n), 0);
   }
-  {
+  { // vectorの書き出し
     std::vector<std::byte> v(test_data, test_data + n);
     BOOST_TEST_EQ(menon::file_put_contents(path, v), n);
     auto data = get_test_file(path);
     BOOST_TEST_EQ(data.size(), n);
     BOOST_TEST_EQ(std::memcmp(data.data(), test_data, n), 0);
   }
-  {
+  { // spanを用いての追記
     menon::file_put_contents(path, test_data);
     menon::file_put_contents(path, std::span<std::byte>(test_data), menon::FILE_APPEND);
     auto data = get_test_file(path);
@@ -76,6 +81,7 @@ void test_file_put_contents(char const* path)
 
 int main()
 {
+  // テストデータの作成
   for (std::size_t i = 0; i < sizeof(test_data); i++)
     test_data[i] = static_cast<std::byte>(std::rand() & 0xff);
 
