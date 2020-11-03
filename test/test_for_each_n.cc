f633c326cbf1d8246d530a113295650faadfb1ed
menonfled <elddiry@gmail.com>
2020-11-03 23:52:39 +0900

    for_each_n関数のテストコードを改善

--- test/test_for_each_n.cc
+++ test/test_for_each_n.cc
@@ -1,28 +1,37 @@
-﻿#include "menon/algorithm.hh"
+﻿// test_for_each_n.cc
+#include "menon/algorithm.hh"
 #include <boost/core/lightweight_test.hpp>
 #include <vector>
 #include <list>
 
-int main()
+void test_for_each_n()
 {
   std::vector v = { 1, 2, 3, 4, 5, };
-  std::list<int> l;
   std::size_t n = v.size();
 
-  menon::for_each_n(v.cbegin(), n, [&l](auto iter, auto i) {
-    l.push_back(iter[i]);
-  });
-  BOOST_TEST_ALL_EQ(l.cbegin(), l.cend(), v.cbegin(), v.cend());
+  { // vectorからlistへのコピー
+    std::list<int> l;
+    menon::for_each_n(v.cbegin(), n, [&l](auto iter, auto i) {
+      l.push_back(iter[i]);
+    });
+    BOOST_TEST_ALL_EQ(l.cbegin(), l.cend(), v.cbegin(), v.cend());
+  }
 
-  menon::for_each_n(v.begin(), n, [&v](auto iter, auto& i) {
-    if (iter[i] == 3)
-    {
-      v.erase(iter + i);
-      --i;
-    }
-  });
-  auto il = { 1, 2, 4, 5, };
-  BOOST_TEST_ALL_EQ(v.cbegin(), v.cend(), il.begin(), il.end());
+  { // vectorから3を除去
+    menon::for_each_n(v.begin(), n, [&v](auto iter, auto& i) {
+      if (iter[i] == 3)
+      {
+        v.erase(iter + i);
+        --i;
+      }
+    });
+    auto il = { 1, 2, 4, 5, };
+    BOOST_TEST_ALL_EQ(v.cbegin(), v.cend(), il.begin(), il.end());
+  }
+}
 
+int main()
+{
+  test_for_each_n();
   return boost::report_errors();
 }
