b57103bbb44a6ddf965e0412683b152e93e1be65
menonfled <elddiry@gmail.com>
2020-11-04 00:16:37 +0900

    test_fseek.ccにコメントを追加

--- test/test_fseek.cc
+++ test/test_fseek.cc
@@ -1,9 +1,11 @@
-﻿#include "menon/io.hh"
+﻿// test_fseek.cc
+#include "menon/io.hh"
 #include <boost/core/lightweight_test.hpp>
 #include <cstddef>
 #include <vector>
 #include <fstream>
 
+// テストデータの作成
 long long make_test_file(char const* path)
 {
   std::vector<std::byte> v(256, std::byte(123));
@@ -20,7 +22,7 @@ int main()
   char const* path = "test.data";
   [[maybe_unused]] auto n = make_test_file(path);
 
-  {
+  { // fseek/ftell関数の基本的な使い方
     auto stream = std::fopen(path, "rb");
     menon::fseek(stream, 100, SEEK_SET);
     BOOST_TEST_EQ(std::ftell(stream), 100);
@@ -28,7 +30,7 @@ int main()
     std::fclose(stream);
   }
 
-  {
+  { // fseek/ftell関数とfsetpos/fgetpos関数の混在
     auto stream = std::fopen(path, "rb");
     menon::fpos_t pos;
     menon::fseek(stream, 100, SEEK_SET);
@@ -39,13 +41,13 @@ int main()
     std::fclose(stream);
   }
 
-  {
+  { // istream版のfseek関数
     std::ifstream ifs(path);
     menon::fseek(ifs, 100, SEEK_SET);
     BOOST_TEST_EQ(ifs.tellg(), 100);
   }
 
-  {
+  { // istream版のfseek関数とfsetpos/fgetos関数の混在
     std::ifstream ifs(path);
     menon::fpos_t pos;
     menon::fseek(ifs, 100, SEEK_SET);
@@ -55,20 +57,20 @@ int main()
     BOOST_TEST_EQ(ifs.tellg(), 100);
   }
 
-  {
+  { // iostream版のfseek関数
     std::fstream fs(path);
     menon::fseek(fs, 100, SEEK_SET);
     BOOST_TEST_EQ(fs.tellp(), 100);
     BOOST_TEST_EQ(fs.tellg(), 100);
   }
 
-  {
+  { // ostream版のfseek関数
     std::ofstream ofs(path);
     menon::fseek(ofs, 100, SEEK_SET);
     BOOST_TEST_EQ(ofs.tellp(), 100);
   }
 
-  {
+  { // ostream版のfseek関数とfsetpos/fgetpos関数の混在
     std::ofstream ofs(path);
     menon::fpos_t pos;
     menon::fseek(ofs, 100, SEEK_SET);
