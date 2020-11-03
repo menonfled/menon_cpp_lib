444cecc8db3a5fb1101d93cfc74fc752fbdba1dc
menonfled <elddiry@gmail.com>
2020-11-03 12:58:00 +0900

    test_find.ccを改善

--- test/test_find.cc
+++ test/test_find.cc
@@ -1,3 +1,4 @@
+// test_find.cc
 #include "menon/algorithm.hh"
 #include <boost/core/lightweight_test.hpp>
 #include <vector>
@@ -7,11 +8,12 @@
 #include <set>
 #include <string>
 
+// find関数のテストコード
 void test_find()
 {
-  {
+  { // vectorに対する探索
     std::vector v = { 3, 2, 7, 1, 9, 5 };
-    {
+    { // find
       auto p = menon::find(v, 7);
       BOOST_TEST(p);
       if (p)
@@ -22,7 +24,7 @@ void test_find()
       p = menon::find(v, 6);
       BOOST_TEST_NOT(p);
     }
-    {
+    { //cfing
       auto p = menon::cfind(v, 7);
       BOOST_TEST(p);
       if (p)
@@ -34,7 +36,7 @@ void test_find()
       BOOST_TEST_NOT(p);
     }
   }
-  {
+  { // dequeに対する探索
     std::deque d = { 3, 2, 7, 1, 9, 5 };
     auto p = menon::find(d, 7);
     BOOST_TEST(p);
@@ -45,7 +47,7 @@ void test_find()
     p = menon::find(d, 6);
     BOOST_TEST_NOT(p);
   }
-  {
+  { // listに対する探索
     std::list l = { 3, 2, 7, 1, 9, 5 };
     auto p = menon::find(l, 7);
     BOOST_TEST(p);
@@ -56,33 +58,35 @@ void test_find()
     p = menon::find(l, 6);
     BOOST_TEST_NOT(p);
   }
-  int a[] = { 3, 2, 7, 1, 9, 5 };
-  {
-    auto p = menon::find(a, 7);
-    BOOST_TEST(p);
-    if (p)
-    {
-      BOOST_TEST_EQ(*p, 7);
-      BOOST_TEST_EQ(p, a + 2);
+  { // 配列に対する探索
+    int a[] = { 3, 2, 7, 1, 9, 5 };
+    { //find
+      auto p = menon::find(a, 7);
+      BOOST_TEST(p);
+      if (p)
+      {
+        BOOST_TEST_EQ(*p, 7);
+        BOOST_TEST_EQ(p, a + 2);
+      }
+      p = menon::find(a, 6);
+      BOOST_TEST_NOT(p);
     }
-    p = menon::find(a, 6);
-    BOOST_TEST_NOT(p);
-  }
-  {
-    auto p = menon::cfind(a, 7);
-    BOOST_TEST(p);
-    if (p)
-    {
-      BOOST_TEST_EQ(*p, 7);
-      BOOST_TEST_EQ(p, a + 2);
+    { // cfind
+      auto p = menon::cfind(a, 7);
+      BOOST_TEST(p);
+      if (p)
+      {
+        BOOST_TEST_EQ(*p, 7);
+        BOOST_TEST_EQ(p, a + 2);
+      }
+      p = menon::cfind(a, 6);
+      BOOST_TEST_NOT(p);
     }
-    p = menon::cfind(a, 6);
-    BOOST_TEST_NOT(p);
   }
 
-  {
+  { // setに対する探索
     std::set s = { 3, 2, 7, 1, 9, 5 };
-    {
+    { // finc
       auto p = menon::find(s, 7);
       BOOST_TEST(p);
       if (p)
@@ -92,7 +96,7 @@ void test_find()
       p = menon::find(s, 6);
       BOOST_TEST_NOT(p);
     }
-    {
+    { // cfind
       auto p = menon::cfind(s, 7);
       BOOST_TEST(p);
       if (p)
@@ -104,32 +108,35 @@ void test_find()
     }
   }
 
-  std::map<std::string, int> m = { { "d", 3 }, { "y", 2 }, { "c", 7 }, { "v", 1 } };
-  {
-    auto p = menon::find(m, "c");
-    BOOST_TEST(p);
-    if (p)
-    {
-      BOOST_TEST_EQ(*p, 7);
+  { // mapに対する探索
+    std::map<std::string, int> m = { { "d", 3 }, { "y", 2 }, { "c", 7 }, { "v", 1 } };
+    { // find
+      auto p = menon::find(m, "c");
+      BOOST_TEST(p);
+      if (p)
+      {
+        BOOST_TEST_EQ(*p, 7);
+      }
+      p = menon::find(m, "k");
+      BOOST_TEST_NOT(p);
     }
-    p = menon::find(m, "k");
-    BOOST_TEST_NOT(p);
-  }
-  {
-    auto p = menon::cfind(m, "c");
-    BOOST_TEST(p);
-    if (p)
-    {
-      BOOST_TEST_EQ(*p, 7);
+    { // cfind
+      auto p = menon::cfind(m, "c");
+      BOOST_TEST(p);
+      if (p)
+      {
+        BOOST_TEST_EQ(*p, 7);
+      }
+      p = menon::cfind(m, "k");
+      BOOST_TEST_NOT(p);
     }
-    p = menon::cfind(m, "k");
-    BOOST_TEST_NOT(p);
   }
 }
 
+// find_if関数のテストコード
 void test_find_if()
 {
-  {
+  { // vectorに対するfind_if
     std::vector v = { 3, 2, 7, 1, 9, 5 };
     {
       auto p = menon::find_if(v, [](auto value) {
@@ -148,7 +155,7 @@ void test_find_if()
     }
   }
 
-  {
+  { // vectorに対するcfind_if
     std::vector v = { 3, 2, 7, 1, 9, 5 };
     {
       auto p = menon::cfind_if(v, [](auto value) {
