fcb5ff80685581bbda7dcbf27820fb7cfa980572
menonfled <elddiry@gmail.com>
2020-11-06 23:08:54 +0900

    test_implode.ccにlist版を追加

--- test/test_implode.cc
+++ test/test_implode.cc
@@ -1,27 +1,30 @@
-﻿#include "menon/string.hh"
+﻿// test_implode.cc
+#include "menon/string.hh"
 #include <boost/core/lightweight_test.hpp>
 #include <vector>
+#include <list>
 
 int main()
 {
   using namespace std::literals::string_view_literals;
   using namespace std::literals::string_literals;
-  {
-    auto t = { "abc"s, "def"s, "ghi"s };
+
+  { // string_viewのvectorをナル終端文字列で連結
+    std::vector t = { "abc"s, "def"s, "ghi"s };
     BOOST_TEST_EQ(menon::implode(",", t), "abc,def,ghi"s);
   }
-  {
+  { // string_viewをstring_viewで連結
     auto t = { "abc"s, "def"s, "ghi"s };
     BOOST_TEST_EQ(menon::implode(","sv, t), "abc,def,ghi"s);
   }
-  {
+  { // string_viewのinitializer_listをナル終端文字列で連結
     BOOST_TEST_EQ(menon::implode(",", { "abc"s, "def"s, "ghi"s }), "abc,def,ghi"s);
   }
-  {
+  { // string_viewのinitializer_listをstring_viewで連結
     BOOST_TEST_EQ(menon::implode(","s, { "abc"s, "def"s, "ghi"s }), "abc,def,ghi"s);
   }
-  {
-    std::vector t = { "abc"s, "def"s, "ghi"s };
+  { // string_viewのlistをナル終端文字列で連結
+    std::list t = { "abc"s, "def"s, "ghi"s };
     BOOST_TEST_EQ(menon::implode(",", t.begin(), t.end()), "abc,def,ghi"s);
   }
   return boost::report_errors();
