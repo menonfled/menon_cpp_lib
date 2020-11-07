e5a85834cbb21f5fb373011474bc456ccee28274
menonfled <elddiry@gmail.com>
2020-11-07 23:21:42 +0900

    test_sv.ccを整理

--- test/test_sv.cc
+++ test/test_sv.cc
@@ -1,4 +1,5 @@
-﻿#include "menon/string.hh"
+﻿// test_sv.cc
+#include "menon/string.hh"
 #include <boost/core/lightweight_test.hpp>
 
 int main()
@@ -7,34 +8,38 @@ int main()
   using namespace std::literals::string_literals;
   char s[] = "abc";
 
+  // 文字列リテラルおよびstring_viewからの変換
   constexpr auto r1 = menon::sv("abc");
   constexpr auto r2 = menon::sv("abc"sv);
   BOOST_TEST_EQ(r1, r2);
   constexpr auto r3 = menon::sv("abc", 2);
   BOOST_TEST_EQ(r3, "ab"s);
-  // constexpr auto r4 = menon::sv('a');
 
+  // 文字からの変換
   BOOST_TEST_EQ(menon::sv('a'), "a"sv);
 
-  BOOST_TEST_EQ(menon::sv("abc", 2), "ab"sv);
+  // nullptrおよび文字配列からの変換
   BOOST_TEST_EQ(menon::sv((char const*)nullptr, 2), ""sv);
   BOOST_TEST_EQ(menon::sv(s, 2), "ab"sv);
 
-  BOOST_TEST_EQ(menon::sv("abc"), "abc"sv);
+  // nullptr、文字配列および空文字列からの変換
   BOOST_TEST_EQ(menon::sv((char const*)nullptr), ""sv);
   BOOST_TEST_EQ(menon::sv(s), "abc"sv);
   BOOST_TEST_EQ(menon::sv(""), ""sv);
 
+  // string_viewおよびstringからの変換
   BOOST_TEST_EQ(menon::sv("abc"sv), "abc"sv);
   BOOST_TEST_EQ(menon::sv("abc"s), "abc"sv);
 #if MENON_HAS_BOOST
+  // boost::string_viewからの変換
   BOOST_TEST_EQ(menon::sv(boost::string_view("abc")), "abc"sv);
 #endif
 #if MENON_HAS_GSL
+  // gsl::string_spanからの変換
   BOOST_TEST_EQ(menon::sv(gsl::basic_string_span<char>(s, 3)), "abc"sv);
 #endif
 
-  {
+  { // sv_to_array関数のテストコード
     constexpr auto sv = "abcde"sv;
     constexpr auto a = menon::sv_to_array<3>("abcde"sv);
     BOOST_TEST_ALL_EQ(a.begin(), a.end(), sv.begin(), sv.begin() + 3);
