b98bd87a358e85d69975dc94cd2d2ea30015f81e
menonfled <elddiry@gmail.com>
2020-11-08 21:53:17 +0900

    test_mb_internal_encoding.ccにコメントを追加

--- test/test_mb_internal_encoding.cc
+++ test/test_mb_internal_encoding.cc
@@ -1,21 +1,31 @@
-﻿#include "menon/encoding.hh"
+﻿// test_mb_internal_encoding.cc
+#include "menon/encoding.hh"
 #include <boost/core/lightweight_test.hpp>
 #include <iostream>
 
 int main()
 {
+  // default_encoding_keyのテストコード
   BOOST_TEST(menon::detail::default_encoding_key >= 0);
   BOOST_TEST_CSTR_EQ(menon::mb_internal_encoding(), menon::detail::encoding_list[menon::detail::default_encoding_key]);
+
 #if defined(_MSC_VER) || (defined(__MINGW32__) && !defined(__clang__))
+  // Windows向けのテストコード
   BOOST_TEST_CSTR_EQ(menon::mb_internal_encoding(), "CP932");
+  // 内部エンコーディングをEUC-JPに設定
   BOOST_TEST_CSTR_EQ(menon::mb_internal_encoding("EUC-JP"), "CP932");
 #else
+  // Windows以外向けのテストコード
   BOOST_TEST_CSTR_EQ(menon::mb_internal_encoding(), "UTF-8");
+  // 内部エンコーディングをEUC-JPに設定
   BOOST_TEST_CSTR_EQ(menon::mb_internal_encoding("EUC-JP"), "UTF-8");
 #endif
+  // 内部エンコーディングEUC-JPになっていることを確認
   BOOST_TEST_CSTR_EQ(menon::mb_internal_encoding(), "EUC-JP");
+  // 不正なエンコーディングを指定した場合のエラー処理
   BOOST_TEST_EQ(menon::mb_internal_encoding("xxx"), nullptr);
 
+  // デフォルトの内部文字エンコーディングに関するテストコード
   constexpr auto char32_encoding = menon::get_internal_encoding<char32_t>();
   constexpr auto char16_encoding = menon::get_internal_encoding<char16_t>();
 #if defined(_MSC_VER) || __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
