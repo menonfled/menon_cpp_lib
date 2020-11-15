cc37b0e42026f6e4b5bfce992ca2c6219fad1e61
menonfled <elddiry@gmail.com>
2020-11-15 18:05:37 +0900

    test_strchr.ccにコメントを追加

--- test/test_strchr.cc
+++ test/test_strchr.cc
@@ -1,13 +1,15 @@
-﻿#include "menon/string.hh"
+﻿// test_strchr.cc
+#include "menon/string.hh"
 #include <boost/core/lightweight_test.hpp>
 #include <cwchar>
 #include <iostream>
 
+// strchr関数のテストコード
 void test_strchr()
 {
   using namespace std::literals;
 
-  {
+  { // char配列版
     constexpr auto s = "abcdefg";
     constexpr auto it = menon::strchr(s, 'd');
     BOOST_TEST_EQ(it, &s[3]);
@@ -15,40 +17,40 @@ void test_strchr()
     BOOST_TEST_EQ(menon::strchr(const_cast<char const*>(&s[0]), 'd'), &s[3]);
     BOOST_TEST_EQ(menon::strchr(s, 'x'), nullptr);
   }
-  {
+  { // string_view版
     constexpr auto s = "abcdefg"sv;
     constexpr auto it = menon::strchr(s, 'd');
     BOOST_TEST_EQ(it, s.begin() + 3);
     BOOST_TEST_EQ(menon::strchr(s, 'x'), decltype(s.begin())());
   }
-  {
+  { // string版
     auto s = "abcdefg"s;
     auto it = menon::strchr(s, 'd');
     BOOST_TEST(it == s.begin() + 3);
     BOOST_TEST(menon::strchr(s, 'x') == decltype(s.begin())());
   }
 
-  {
+  { // wchar_t配列版
     constexpr auto s = L"abcdefg";
     constexpr auto it = menon::strchr(s, L'd');
     BOOST_TEST(it == &s[3]);
     BOOST_TEST(menon::strchr(&s[0], L'd') == &s[3]);
     BOOST_TEST(menon::strchr(s, L'x') == nullptr);
   }
-  {
+  { // wchar_t配列とchar文字版
     constexpr auto s = L"abcdefg";
     constexpr auto it = menon::strchr(s, 'd');
     BOOST_TEST(it == &s[3]);
     BOOST_TEST(menon::strchr(&s[0], 'd') == &s[3]);
     BOOST_TEST(menon::strchr(s, 'x') == nullptr);
   }
-  {
+  { // wstring_view版
     constexpr auto s = L"abcdefg"sv;
     constexpr auto it = menon::strchr(s, L'd');
     BOOST_TEST(it == s.begin() + 3);
     BOOST_TEST(menon::strchr(s, L'x') == decltype(s.begin())());
   }
-  {
+  { // wstring版
     auto s = L"abcdefg"s;
     auto it = menon::strchr(s, L'd');
     BOOST_TEST(it == s.begin() + 3);
@@ -56,11 +58,12 @@ void test_strchr()
   }
 }
 
+// strrchr関数のテストコード
 void test_strrchr()
 {
   using namespace std::literals;
 
-  {
+  { // char配列版
     constexpr auto s = "abcabcabc";
     constexpr auto it = menon::strrchr(s, 'b');
     BOOST_TEST_EQ(it, &s[7]);
@@ -68,13 +71,13 @@ void test_strrchr()
     BOOST_TEST_EQ(menon::strrchr(const_cast<char const*>(&s[0]), 'a'), &s[6]);
     BOOST_TEST_EQ(menon::strrchr(s, 'x'), nullptr);
   }
-  {
+  { // string_view版
     constexpr auto s = "abcabcabc"sv;
     constexpr auto it = menon::strrchr(s, 'b');
     BOOST_TEST_EQ(it, s.begin() + 7);
     BOOST_TEST_EQ(menon::strrchr(s, 'x'), decltype(s.begin())());
   }
-  {
+  { // string版
     auto s = "abcabcabc"s;
     auto it = menon::strrchr(s, 'a');
     BOOST_TEST(it == s.begin() + 6);
