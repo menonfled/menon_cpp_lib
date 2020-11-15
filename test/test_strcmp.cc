1a08cd24b1fd82af05b6a471078f49261d242220
menonfled <elddiry@gmail.com>
2020-11-15 21:41:26 +0900

    test_strcmp.ccにコメントを追加

--- test/test_strcmp.cc
+++ test/test_strcmp.cc
@@ -1,21 +1,26 @@
-﻿#include "menon/string.hh"
+﻿// test_strcmp.cc
+#include "menon/string.hh"
 #include <boost/core/lightweight_test.hpp>
 
 int main()
 {
+  // ナロー文字列のテストコード
   {
     std::string s = "abcdefghi";
     std::string_view sv = s;
 
+    // strcmp関数のテストコード
     BOOST_TEST(menon::strcmp(s, sv) == 0);
     BOOST_TEST(menon::strcmp(s, "fgnrk") < 0);
     BOOST_TEST(menon::strcmp(s, "aaekj") > 0);
 
+    // strncmp関数のテストコード
     BOOST_TEST(menon::strncmp(s, sv, 5) == 0);
     BOOST_TEST(menon::strncmp(s, "abcrtylk", 3) == 0);
     BOOST_TEST(menon::strncmp(s, "fgnrk", 5) < 0);
     BOOST_TEST(menon::strncmp(s, "aaekj", 10) > 0);
 
+    // strcasecmp関数のテストコード
     BOOST_TEST(menon::strcasecmp(s, sv) == 0);
     BOOST_TEST(menon::strcasecmp(s, "fgnrk") < 0);
     BOOST_TEST(menon::strcasecmp(s, "aaekj") > 0);
@@ -23,6 +28,7 @@ int main()
     BOOST_TEST(menon::strcasecmp(s, "FgNrk") < 0);
     BOOST_TEST(menon::strcasecmp(s, "aAeKJ") > 0);
 
+    // strncasecmp関数のテストコード
     BOOST_TEST(menon::strncasecmp(s, sv, 5) == 0);
     BOOST_TEST(menon::strncasecmp(s, "AbCrTyLK", 3) == 0);
     BOOST_TEST(menon::strncasecmp(s, "fgnrk", 5) < 0);
@@ -31,19 +37,24 @@ int main()
     BOOST_TEST(menon::strncasecmp(s, "FgNrk", 5) < 0);
     BOOST_TEST(menon::strncasecmp(s, "aAeKJ", 10) > 0);
   }
+
+  // ワイド文字列のテストコード
   {
     std::wstring s = L"abcdefghi";
     std::wstring_view sv = s;
 
+    // strcmp関数のテストコード
     BOOST_TEST(menon::strcmp(s, sv) == 0);
     BOOST_TEST(menon::strcmp(s, L"fgnrk") < 0);
     BOOST_TEST(menon::strcmp(s, L"aaekj") > 0);
 
+    // strncmp関数のテストコード
     BOOST_TEST(menon::strncmp(s, sv, 5) == 0);
     BOOST_TEST(menon::strncmp(s, L"abcrtylk", 3) == 0);
     BOOST_TEST(menon::strncmp(s, L"fgnrk", 5) < 0);
     BOOST_TEST(menon::strncmp(s, L"aaekj", 10) > 0);
 
+    // strcasecmp関数のテストコード
     BOOST_TEST(menon::strcasecmp(s, sv) == 0);
     BOOST_TEST(menon::strcasecmp(s, L"fgnrk") < 0);
     BOOST_TEST(menon::strcasecmp(s, L"aaekj") > 0);
@@ -51,6 +62,7 @@ int main()
     BOOST_TEST(menon::strcasecmp(s, L"FgNrk") < 0);
     BOOST_TEST(menon::strcasecmp(s, L"aAeKJ") > 0);
 
+    // strncasecmp関数のテストコード
     BOOST_TEST(menon::strncasecmp(s, sv, 5) == 0);
     BOOST_TEST(menon::strncasecmp(s, L"AbCrTyLK", 3) == 0);
     BOOST_TEST(menon::strncasecmp(s, L"fgnrk", 5) < 0);
