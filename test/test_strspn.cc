9f70ea343d985f80a89cd84896db0416644d78ac
menonfled <elddiry@gmail.com>
2020-11-17 00:13:01 +0900

    test_strspn.ccにコメントを追加

--- test/test_strspn.cc
+++ test/test_strspn.cc
@@ -1,29 +1,31 @@
-﻿#include "menon/string.hh"
+﻿// strspn.cc
+#include "menon/string.hh"
 #include <boost/core/lightweight_test.hpp>
 
+// strspn関数のテストコード
 void test_strspn()
 {
   using namespace std::literals;
 
-  {
+  { // char配列版
     constexpr auto s1 = "aaaaabbbcc";
     constexpr auto s2 = "ab";
     constexpr auto r = menon::strspn(s1, s2);
     BOOST_TEST_EQ(r, std::strspn(s1, s2));
   }
-  {
+  { // string_view版
     constexpr auto s1 = "aaaaabbbcc"sv;
     constexpr auto s2 = "ab"sv;
     constexpr auto r = menon::strspn(s1, s2);
     BOOST_TEST_EQ(r, std::strspn("aaaaabbbcc", "ab"));
   }
-  {
+  { // stringおよびchar配列版
     auto s1 = "aaaaabbbcc"s;
     auto s2 = "ab";
     auto r = menon::strspn(s1, s2);
     BOOST_TEST_EQ(r, std::strspn(s1.c_str(), s2));
   }
-  {
+  { // char配列およびstring版
     constexpr auto s1 = "aaaaabbbcc";
     auto s2 = "ab"s;
     auto r = menon::strspn(s1, s2);
@@ -31,29 +33,30 @@ void test_strspn()
   }
 }
 
+// strcspn関数のテストコード
 void test_strcspn()
 {
   using namespace std::literals;
 
-  {
+  { // char配列版
     constexpr auto s1 = "aaaaabbbcc";
     constexpr auto s2 = "ab";
     constexpr auto r = menon::strcspn(s1, s2);
     BOOST_TEST_EQ(r, std::strcspn(s1, s2));
   }
-  {
+  { // string_view版
     constexpr auto s1 = "aaaaabbbcc"sv;
     constexpr auto s2 = "ab"sv;
     constexpr auto r = menon::strcspn(s1, s2);
     BOOST_TEST_EQ(r, std::strcspn("aaaaabbbcc", "ab"));
   }
-  {
+  { // stringおよびchar配列版
     auto s1 = "aaaaabbbcc"s;
     auto s2 = "ab";
     auto r = menon::strcspn(s1, s2);
     BOOST_TEST_EQ(r, std::strcspn(s1.c_str(), s2));
   }
-  {
+  { // char配列およびstring版
     constexpr auto s1 = "aaaaabbbcc";
     auto s2 = "ab"s;
     auto r = menon::strcspn(s1, s2);
