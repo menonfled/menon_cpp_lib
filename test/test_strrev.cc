48c55073de407f82efcd66fdb71365cec3835f2f
menonfled <elddiry@gmail.com>
2020-11-17 00:13:29 +0900

    test_strrev.ccを改善

--- test/test_strrev.cc
+++ test/test_strrev.cc
@@ -1,21 +1,26 @@
-﻿#include "menon/string.hh"
+﻿// test_strrev.cc
+#include "menon/string.hh"
 #include <boost/core/lightweight_test.hpp>
 
 int main()
 {
   using namespace std::literals;
 
-  BOOST_TEST_EQ(menon::strrev("abcde"), "edcba");
-  BOOST_TEST_EQ(menon::strrev("abcde"s), "edcba");
-  BOOST_TEST_EQ(menon::strrev("abcde"sv), "edcba");
-  char const* s = "abcde";
-  BOOST_TEST_EQ(menon::strrev(s), "edcba");
+  { // ナロー文字列版
+    BOOST_TEST_EQ(menon::strrev("abcde"), "edcba");
+    BOOST_TEST_EQ(menon::strrev("abcde"s), "edcba");
+    BOOST_TEST_EQ(menon::strrev("abcde"sv), "edcba");
+    char const* s = "abcde";
+    BOOST_TEST_EQ(menon::strrev(s), "edcba");
+  }
 
-  BOOST_TEST(menon::strrev(L"abcde") == L"edcba");
-  BOOST_TEST(menon::strrev(L"abcde"s) == L"edcba");
-  BOOST_TEST(menon::strrev(L"abcde"sv) == L"edcba");
-  wchar_t const* wcs = L"abcde";
-  BOOST_TEST(menon::strrev(wcs) == L"edcba");
+  { // ワイド文字列版
+    BOOST_TEST(menon::strrev(L"abcde") == L"edcba");
+    BOOST_TEST(menon::strrev(L"abcde"s) == L"edcba");
+    BOOST_TEST(menon::strrev(L"abcde"sv) == L"edcba");
+    wchar_t const* wcs = L"abcde";
+    BOOST_TEST(menon::strrev(wcs) == L"edcba");
+  }
 
   return boost::report_errors();
 }
