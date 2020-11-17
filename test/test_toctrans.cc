002a5cb4656541ea96ae3e1707954fe677fe359a
menonfled <elddiry@gmail.com>
2020-11-18 00:16:07 +0900

    test_toctrans.ccを改善

--- test/test_toctrans.cc
+++ test/test_toctrans.cc
@@ -1,9 +1,13 @@
-﻿#include "menon/ctype.hh"
+﻿// test_toctrans.cc
+#include "menon/ctype.hh"
 #include <boost/core/lightweight_test.hpp>
 #include <cctype>
 
+using namespace std::literals;
+
+// to系関数のテストコード
 template <typename Char>
-void test()
+void test_to()
 {
   for (int i = -1; i <= 0x80; i++)
   {
@@ -19,18 +23,16 @@ void test()
   }
 }
 
-int main()
+// ctarns関数のテストコード
+void test_ctrans()
 {
-  test<char>();
-  test<wchar_t>();
-  test<char8_t>();
-  test<char16_t>();
-  test<char32_t>();
   BOOST_TEST(menon::ctrans("tolower") == menon::tolower<char>);
   BOOST_TEST(menon::ctrans("toupper") == menon::toupper<char>);
- 
-  using namespace std::literals;
+}
 
+// strtolower関数のテストコード
+void test_strtolower()
+{
   BOOST_TEST_EQ(menon::strtolower("0123456789AbcDEfgH"), "0123456789abcdefgh");
   BOOST_TEST(menon::strtolower(u8"0123456789AbcDEfgH") == u8"0123456789abcdefgh");
   BOOST_TEST(menon::strtolower(L"0123456789AbcDEfgH") == L"0123456789abcdefgh");
@@ -46,7 +48,11 @@ int main()
   BOOST_TEST(menon::strtolower(L"0123456789AbcDEfgH"s) == L"0123456789abcdefgh");
   BOOST_TEST(menon::strtolower(u"0123456789AbcDEfgH"s) == u"0123456789abcdefgh");
   BOOST_TEST(menon::strtolower(U"0123456789AbcDEfgH"s) == U"0123456789abcdefgh");
+}
 
+// strtoupper関数のテストコード
+void test_strtoupper()
+{
   BOOST_TEST_EQ(menon::strtoupper("0123456789AbcDEfgH"), "0123456789ABCDEFGH");
   BOOST_TEST(menon::strtoupper(u8"0123456789AbcDEfgH") == u8"0123456789ABCDEFGH");
   BOOST_TEST(menon::strtoupper(L"0123456789AbcDEfgH") == L"0123456789ABCDEFGH");
@@ -62,6 +68,20 @@ int main()
   BOOST_TEST(menon::strtoupper(L"0123456789AbcDEfgH"s) == L"0123456789ABCDEFGH");
   BOOST_TEST(menon::strtoupper(u"0123456789AbcDEfgH"s) == u"0123456789ABCDEFGH");
   BOOST_TEST(menon::strtoupper(U"0123456789AbcDEfgH"s) == U"0123456789ABCDEFGH");
+}
+
+int main()
+{
+  test_to<char>();
+  test_to<wchar_t>();
+  test_to<char8_t>();
+  test_to<char16_t>();
+  test_to<char32_t>();
+
+  test_ctrans();
+
+  test_strtolower();
+  test_strtoupper();
 
   return boost::report_errors();
 }
