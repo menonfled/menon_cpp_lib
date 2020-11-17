0a519fdaabea48d51cbb9145455bbec9bbd472bc
menonfled <elddiry@gmail.com>
2020-11-18 00:18:15 +0900

    test_trim.ccを改善

--- test/test_trim.cc
+++ test/test_trim.cc
@@ -1,18 +1,25 @@
-﻿#include "menon/string.hh"
+﻿// test_trim.cc
+#include "menon/string.hh"
 #include <boost/core/lightweight_test.hpp>
 
-int main()
-{
-  using namespace std::literals;
+using namespace std::literals;
 
+void test_ltrim()
+{
   BOOST_TEST_EQ(menon::ltrim("abcde", "ab"), "cde");
   BOOST_TEST_EQ(menon::ltrim(" \r\n\tabcde"), "abcde");
   BOOST_TEST_EQ(menon::ltrim("abcde", "xy"), "abcde");
+}
 
+void test_rtrim()
+{
   BOOST_TEST_EQ(menon::rtrim("abcde", "ed"), "abc");
   BOOST_TEST_EQ(menon::rtrim("abcde \r\n\t"), "abcde");
   BOOST_TEST_EQ(menon::rtrim("abcde", "xy"), "abcde");
+}
 
+void test_trim()
+{
   BOOST_TEST_EQ(menon::trim("abcde", "ab"), "cde");
   BOOST_TEST_EQ(menon::trim(" \r\n\tabcde"), "abcde");
   BOOST_TEST_EQ(menon::trim("abcde", "xy"), "abcde");
@@ -21,6 +28,12 @@ int main()
   BOOST_TEST_EQ(menon::trim("abcde", "xy"), "abcde");
   BOOST_TEST_EQ(menon::trim("abcde", "eda"), "bc");
   BOOST_TEST_EQ(menon::trim(" \r\n\tabcde \r\n\t"), "abcde");
+}
 
+int main()
+{
+  test_ltrim();
+  test_rtrim();
+  test_trim();
   return boost::report_errors();
 }
